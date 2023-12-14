ba024387ff6b52a01f3d6da521013d3609289ca7
choiyj <choiyj@vp.co.kr>
2023-12-14 16:47:49 +0900

    초대코드 생성View 초안

--- Projects/App/Sources/Feature/Setting/View/InviteCodeView.swift
+++ Projects/App/Sources/Feature/Setting/View/InviteCodeView.swift
@@ -7,13 +7,36 @@
 //
 
 import SwiftUI
+import GetCKit
 
 import ComposableArchitecture
 import TCACoordinators
 
 struct InviteCodeView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack(alignment: .leading) {
+            Spacer()
+                .frame(height: 30)
+            Text("친구를 초대해 보세요!")
+                .fontWeight(.bold)
+            Text("총 3명의 친구를 초대할 수 있어요")
+            
+        Spacer()
+                .frame(height: 26)
+            
+            Button {
+                
+            } label: {
+                Text("초대코드 생성")
+            }
+            .buttonStyle(CommonButtonStyle())
+            Spacer()
+            
+        }
+        .padding(.horizontal, GetCGridRules.globalHorizontalPadding)
     }
 }
 
+#Preview {
+    InviteCodeView()
+}
